1bdf9a03a1e6cefab8231b94d5665bbfea13f0e2
Jeremiah Naylor-Trein <jeremiahtrein@gmail.com>
2021-01-07 15:02:48 +0000

    adds VOTE_LENGTH_SECONDS web3 config

--- src/components/web3/config.ts
+++ src/components/web3/config.ts
@@ -4,6 +4,8 @@ import {CoreProposalVoteChoices, VoteChoices} from './types';
  * WEB3 CONFIG for TRIBUTE DAO
  */
 
+export const VOTE_LENGTH_SECONDS: number = 180; // 3 minutes
+
 // Vote choices should be "yes", "no" unless Moloch v3 contracts change.
 export const VOTE_CHOICES: CoreProposalVoteChoices = [
   VoteChoices.yes,
