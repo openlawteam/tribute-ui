58862b64d57077b1dd5726ab3968a34040b08565
Jeremiah Naylor-Trein <jeremiahtrein@gmail.com>
2021-01-07 13:15:19 +0000

    adds core proposal data types

--- src/components/web3/types.ts
+++ src/components/web3/types.ts
@@ -1,6 +1,12 @@
 import {AbiItem} from 'web3-utils/types';
 import {Contract as Web3Contract} from 'web3-eth-contract/types';
 
+/**
+ * WEB3 TYPES
+ */
+
+// CONTRACTS
+
 export type SmartContractItem = {
   instance: Web3Contract;
   abi: AbiItem[];
@@ -13,6 +19,76 @@ export type SmartContracts = {
   OffchainVotingContract: SmartContractItem;
   OnboardingContract: SmartContractItem;
 };
+
+// PROPOSALS
+
+/**
+ * For the proposal `payload` key.
+ * Required by both Moloch and Snapshot proposals
+ */
+export type CoreProposalPayload = {
+  name: string;
+  body: string;
+  choices: CoreProposalVoteChoices;
+  /**
+   * Date timestamp in seconds
+   */
+  start: number;
+  /**
+   * Date timestamp in seconds
+   */
+  end: number;
+  /**
+   * ETH block number coerced to `string`.
+   */
+  snapshot: string;
+};
+
+// Required by both Moloch and Snapshot proposals
+export type CoreProposalData = {
+  payload: CoreProposalPayload;
+  /**
+   * The ERC712 signature `string` returned from a signing function.
+   */
+  sig: string;
+  /**
+   * Space is a unique key (typically a contract address)
+   * used by Moloch and Snapshot for building core proposal data.
+   *
+   * It is also used inside a Snapshot Hub for matching a `space`
+   * with its own proposals and votes.
+   */
+  space: string;
+  /**
+   * Date timestamp in seconds
+   */
+  timestamp: number;
+  type: CoreProposalType;
+};
+
+export type CoreProposalType = 'proposal' | 'result' | 'vote';
+
+// Ordered vote choices. Do not change the indexes!
+export type CoreProposalVoteChoices = [VoteChoices.yes, VoteChoices.no];
+
+/**
+ * WEB3 ENUMS
+ */
+
+export enum ContractAdapterNames {
+  configuration = 'configuration',
+  financing = 'financing',
+  guildkick = 'guildkick',
+  managing = 'managing',
+  onboarding = 'onboarding',
+  ragequit = 'ragequit',
+  voting = 'voting',
+}
+
+export enum VoteChoices {
+  no = 'no',
+  yes = 'yes',
+}
 
 export enum Web3State {
   Connected = 'Wallet connected',
@@ -27,14 +103,4 @@ export enum Web3TxStatus {
   PENDING = 'PENDING',
   FULFILLED = 'FULFILLED',
   REJECTED = 'REJECTED',
-}
-
-export enum ContractAdapterNames {
-  configuration = 'configuration',
-  financing = 'financing',
-  guildkick = 'guildkick',
-  managing = 'managing',
-  onboarding = 'onboarding',
-  ragequit = 'ragequit',
-  voting = 'voting',
 }
