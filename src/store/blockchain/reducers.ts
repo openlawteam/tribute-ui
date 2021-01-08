7404e8e30c9e1f5c368c8a759942eb1e9d594b9a
Jeremiah Naylor-Trein <jeremiahtrein@gmail.com>
2021-01-08 17:59:33 +0000

    cleans up remaining blockchain state we do not need

--- src/store/blockchain/reducers.ts
+++ src/store/blockchain/reducers.ts
@@ -1,8 +1,4 @@
-import {
-  BLOCKCHAIN_CONTRACTS,
-  BLOCKCHAIN_WEB3_STATE,
-  CONNECTED_ADDRESS,
-} from '../actions';
+import {BLOCKCHAIN_CONTRACTS} from '../actions';
 
 import {BlockchainState} from '../../util/types';
 
@@ -17,23 +13,11 @@ export default function reducer(
   switch (type) {
     case BLOCKCHAIN_CONTRACTS:
       return smartContracts(state, payload);
-    case BLOCKCHAIN_WEB3_STATE:
-      return web3State(state, payload);
-    case CONNECTED_ADDRESS:
-      return connectedAddress(state, payload);
     default:
       return state;
   }
 }
 
-function connectedAddress(state: BlockchainState, {connectedAddress}: any) {
-  return {...state, connectedAddress};
-}
-
 function smartContracts(state: BlockchainState, payload: any) {
   return {...state, contracts: {...state.contracts, ...payload.contracts}};
-}
-
-function web3State(state: BlockchainState, {web3State}: any) {
-  return {...state, web3State};
 }
