736bb34ca01db4a79db84cf11e0333175d7ee605
sophiacodes <sophiacodes@gmail.com>
2021-01-13 18:32:22 +0000

    reduced cube size for mobile

--- src/pages/start/GetStarted.tsx
+++ src/pages/start/GetStarted.tsx
@@ -26,46 +26,11 @@ export default function GetStarted() {
           </div>
 
           <div className="landing__img">
-            {/** @note TEMP SPAN */}
             <div className="cube">
               <div className="cube__segment--top"></div>
               <div className="cube__segment--left"></div>
               <div className="cube__segment--right"></div>
             </div>
-
-            {/* <div className="cube">
-              <div className="cube__segment--top">
-                <div>
-                  <h2>Top cube face</h2>
-                  <p>
-                    The top face is nested in an extra div tag to give correct
-                    rotation of skewed rectangle.
-                  </p>
-                  <p>
-                    This face is also scaled, so the font size has been reduced
-                    to accommodate.
-                  </p>
-                </div>
-              </div>
-              <div className="cube__segment--left">
-                <h2>Left cube face</h2>
-                <p>
-                  Duis aute irure dolor in reprehenderit in voluptate velit esse
-                  cillum dolore eu fugiat nulla pariatur. Excepteur sint
-                  occaecat cupidatat non proident, sunt in culpa qui officia
-                  deserunt mollit anim id est laborum.
-                </p>
-              </div>
-              <div className="cube__segment--right">
-                <h2>Right cube face</h2>
-                <p>
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed
-                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
-                  laboris nisi ut aliquip ex ea commodo consequat.
-                </p>
-              </div>
-            </div> */}
           </div>
 
           <div className="landing__button">
