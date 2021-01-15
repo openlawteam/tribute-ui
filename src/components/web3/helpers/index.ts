60d864676fcfe4ba5858726eb12ae3b7b88c678d
Jeremiah Naylor-Trein <jeremiahtrein@gmail.com>
2021-01-15 16:14:16 +0000

    removes broken import

--- src/components/web3/helpers/index.ts
+++ src/components/web3/helpers/index.ts
@@ -2,4 +2,3 @@ export * from './contractSend';
 export * from './getAdapterAddress';
 export * from './getAdapterAddressFromContracts';
 export * from './getDAOConfigEntry';
-export * from './getDefaultProposalBody';
