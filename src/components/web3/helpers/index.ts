a93980a8ae0954ba8fb66666f2925eeb213e075d
Jeremiah Naylor-Trein <jeremiahtrein@gmail.com>
2021-01-15 15:53:35 +0000

    updates helpers index

--- src/components/web3/helpers/index.ts
+++ src/components/web3/helpers/index.ts
@@ -1,3 +1,5 @@
 export * from './contractSend';
 export * from './getAdapterAddress';
+export * from './getAdapterAddressFromContracts';
 export * from './getDAOConfigEntry';
+export * from './getDefaultProposalBody';
