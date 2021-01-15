c91a8fe4ad76624f066a140a510260b0bf1fffda
Jeremiah Naylor-Trein <jeremiahtrein@gmail.com>
2021-01-15 15:52:57 +0000

    adds getAdapterAddressFromContracts helper

---
+++ src/components/web3/helpers/getAdapterAddressFromContracts.ts
@@ -0,0 +1,50 @@
+import {StoreState} from '../../../util/types';
+import {SmartContractItem, ContractAdapterNames} from '../types';
+
+function getContractAddressOrThrow(contract: SmartContractItem | null): string {
+  try {
+    const address = contract?.contractAddress;
+
+    if (!address) {
+      throw new Error(`Contract address was not found in the store.`);
+    }
+
+    return address;
+  } catch (error) {
+    throw error;
+  }
+}
+
+/**
+ * getAdapterAddressFromContracts
+ *
+ * Gets an adapter's address by its name (`type ContractAdapterNames`) from the Redux contracts state slice.
+ *
+ * @param {string} name
+ * @param {StoreState['contracts']} contracts
+ */
+export function getAdapterAddressFromContracts(
+  name: ContractAdapterNames,
+  contracts: StoreState['contracts']
+): string {
+  try {
+    switch (name) {
+      case ContractAdapterNames.configuration:
+        return '';
+      case ContractAdapterNames.financing:
+        return '';
+      case ContractAdapterNames.guildkick:
+        return '';
+      case ContractAdapterNames.managing:
+        return '';
+      case ContractAdapterNames.onboarding:
+        return getContractAddressOrThrow(contracts.OnboardingContract);
+      case ContractAdapterNames.ragequit:
+        return '';
+      default:
+        throw new Error(`Contract address was not found in the store.`);
+    }
+  } catch (error) {
+    throw error;
+  }
+}
