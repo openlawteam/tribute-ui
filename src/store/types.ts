89530585d00d1a6b7d4dae1915aed3dd7b9300d6
Jeremiah Naylor-Trein <jeremiahtrein@gmail.com>
2021-01-26 16:08:55 +0000

    adds store-specific types

---
+++ src/store/types.ts
@@ -0,0 +1,25 @@
+import {Action} from 'redux';
+import {ThunkDispatch} from 'redux-thunk';
+import {SmartContractItem} from '../components/web3/types';
+
+export type ContractsState = {
+  DaoRegistryContract: SmartContractItem | null;
+  OnboardingContract: SmartContractItem | null;
+  OffchainVotingContract: SmartContractItem | null;
+};
+
+export type ConnectedMemberState = {} | null;
+
+export type StoreState = {
+  connectedMemberState: ConnectedMemberState;
+  contracts: ContractsState;
+};
+
+/**
+ * Used when using useDispatch hook.
+ *
+ * e.g. useDispatch<ReduxDispatch>();
+ *
+ * @see https://www.reddit.com/r/typescript/comments/c04mjt/how_to_type_reduxthunks_with_the_new_usedispatch/
+ */
+export type ReduxDispatch = ThunkDispatch<StoreState, any, Action>;
