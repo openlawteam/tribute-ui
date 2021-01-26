7e93531f12245d38f8b914c795bc5b76d506335e
Jeremiah Naylor-Trein <jeremiahtrein@gmail.com>
2021-01-26 13:00:29 +0000

    adds getContractByAddress helper and unit test

---
+++ src/components/web3/helpers/getContractByAddress.unit.test.ts
@@ -0,0 +1,67 @@
+import {waitFor} from '@testing-library/react';
+
+import {DEFAULT_ETH_ADDRESS, setupHook} from '../../../test/helpers';
+import {getContractByAddress} from '.';
+
+describe('getContractByAddress unit tests', () => {
+  test('should return contract data', async () => {
+    /**
+     * @note Not using our `setupHook` this to extract hook data, we will use `renderHook` for this hook test.
+     *   But we still need access to a contract ABI to test `contractSend` properly.
+     *   Hence, the access of the Redux `contract` state.
+     */
+    const {store} = setupHook({
+      wrapperProps: {
+        useInit: true,
+        useWallet: true,
+      },
+    });
+
+    // Wait for contracts to load
+    await waitFor(() => {
+      Object.keys(store.getState().contracts).forEach((c) => {
+        expect(c).not.toBeUndefined();
+        expect(c).not.toBeNull();
+      });
+    });
+
+    const {contractAddress, abi, instance} = getContractByAddress(
+      DEFAULT_ETH_ADDRESS,
+      store.getState().contracts
+    );
+
+    expect(contractAddress).toBe(DEFAULT_ETH_ADDRESS);
+    expect(Object.keys(abi).length).toBeGreaterThan(0);
+    expect(Object.keys(instance).length).toBeGreaterThan(0);
+  });
+
+  test('should throw if contract not found', async () => {
+    /**
+     * @note Not using our `setupHook` this to extract hook data, we will use `renderHook` for this hook test.
+     *   But we still need access to a contract ABI to test `contractSend` properly.
+     *   Hence, the access of the Redux `contract` state.
+     */
+    const {store} = setupHook({
+      wrapperProps: {
+        useInit: true,
+        useWallet: true,
+      },
+    });
+
+    // Wait for contracts to load
+    await waitFor(() => {
+      Object.keys(store.getState().contracts).forEach((c) => {
+        expect(c).not.toBeUndefined();
+        expect(c).not.toBeNull();
+      });
+    });
+
+    // Should throw if not found
+    expect(() =>
+      getContractByAddress(
+        '0x000000000000000000000000000000000000dead',
+        store.getState().contracts
+      )
+    ).toThrow();
+  });
+});
