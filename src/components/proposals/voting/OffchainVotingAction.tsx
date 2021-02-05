b83f5b89e50bd3cfaf7703a921b4d1fc27aff6e5
Jeremiah Naylor-Trein <jeremiahtrein@gmail.com>
2021-02-05 15:00:50 +0000

    adds error, progress, disabled reasons to OffchainVotingAction

--- src/components/proposals/voting/OffchainVotingAction.tsx
+++ src/components/proposals/voting/OffchainVotingAction.tsx
@@ -1,15 +1,29 @@
-import React from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 
-import {ContractAdapterNames} from '../../web3/types';
+import {ContractAdapterNames, Web3TxStatus} from '../../web3/types';
 import {ProposalData} from '../types';
+import {StoreState} from '../../../store/types';
+import {useIsAddressDelegated, useWeb3Modal} from '../../web3/hooks';
+import {useMemberActionDisabled} from '../../../hooks';
+import {useSelector} from 'react-redux';
 import {useSignAndSendVote, useVotingStartEnd} from '../hooks';
 import {VoteChoices} from '@openlaw/snapshot-js-erc712';
 import {VotingActionButtons} from '.';
+import ErrorMessageWithDetails from '../../common/ErrorMessageWithDetails';
+import {getVoteChosen} from '../helpers';
 
 type OffchainVotingActionProps = {
   adapterName: ContractAdapterNames;
   proposal: ProposalData;
 };
+
+type VotingDisabledReasons = {
+  addressIsDelegatedMessage: string;
+  alreadyVotedMessage: string;
+};
+
+const getDelegatedAddressMessage = (a: string) =>
+  `Your member address is delegated to ${a}. You must use that address to vote.`;
 
 /**
  * OffchainVotingAction
@@ -24,17 +38,103 @@ export function OffchainVotingAction(
   const {adapterName, proposal} = props;
 
   /**
+   * State
+   */
+
+  const [voteChoiceClicked, setVoteChoiceClicked] = useState<VoteChoices>();
+  const [submitError, setSubmitError] = useState<Error>();
+
+  /**
+   * Refs
+   */
+
+  const votingDisabledReasonsRef = useRef<VotingDisabledReasons>({
+    addressIsDelegatedMessage: '',
+    alreadyVotedMessage: '',
+  });
+
+  /**
+   * Selectors
+   */
+
+  const delegateAddress = useSelector(
+    (s: StoreState) => s.connectedMember?.delegateKey
+  );
+
+  /**
    * Our hooks
    */
 
+  const {account} = useWeb3Modal();
   const {hasVotingEnded, votingStartEndInitReady} = useVotingStartEnd(proposal);
-  const {signAndSendVote} = useSignAndSendVote();
+  const {signAndSendVote, voteDataStatus} = useSignAndSendVote();
+  const isAddressDelegated = useIsAddressDelegated();
+  const voteChosen = getVoteChosen(
+    proposal.snapshotProposal?.votes,
+    account || ''
+  );
+  const {
+    isDisabled,
+    openWhyDisabledModal,
+    setOtherDisabledReasons,
+    WhyDisabledModal,
+  } = useMemberActionDisabled();
 
   /**
    * Variables
    */
 
   const proposalHash: string = proposal.snapshotProposal?.idInSnapshot || '';
+
+  const isInProcess =
+    voteDataStatus === Web3TxStatus.AWAITING_CONFIRM ||
+    voteDataStatus === Web3TxStatus.PENDING;
+
+  const isDone = voteDataStatus === Web3TxStatus.FULFILLED;
+
+  const isInProcessOrDone = isInProcess || isDone;
+
+  const isSubmitDisabled = isDisabled || isInProcessOrDone;
+
+  const voteChoiceProgress: VoteChoices | undefined = isInProcessOrDone
+    ? voteChoiceClicked
+    : undefined;
+
+  /**
+   * Effects
+   */
+
+  useEffect(() => {
+    // 1. Determine and set local reasons why voting would be disabled
+
+    // Reason: address is delegated
+    if (delegateAddress) {
+      // Set or unset
+      const addressIsDelegatedMessage = isAddressDelegated
+        ? getDelegatedAddressMessage(delegateAddress)
+        : '';
+
+      votingDisabledReasonsRef.current = {
+        ...votingDisabledReasonsRef.current,
+        addressIsDelegatedMessage,
+      };
+    }
+
+    // Reason: already voted
+    votingDisabledReasonsRef.current = {
+      ...votingDisabledReasonsRef.current,
+      alreadyVotedMessage: voteChosen ? 'You have already voted.' : '',
+    };
+
+    // 2. Set reasons
+    setOtherDisabledReasons(Object.values(votingDisabledReasonsRef.current));
+  }, [
+    delegateAddress,
+    hasVotingEnded,
+    isAddressDelegated,
+    setOtherDisabledReasons,
+    voteChosen,
+  ]);
 
   /**
    * Render
@@ -49,32 +149,43 @@ export function OffchainVotingAction(
    */
 
   async function handleSubmitVote(choice: VoteChoices) {
-    if (!proposalHash) {
-      // @todo set error
-      return;
+    try {
+      if (!proposalHash) {
+        throw new Error('No proposal hash was found.');
+      }
+
+      setVoteChoiceClicked(VoteChoices[choice]);
+
+      await signAndSendVote({choice}, adapterName, proposalHash);
+    } catch (error) {
+      setSubmitError(error);
     }
-
-    // 1. Sign and submit to Snapshot Hub
-    const {signature} = await signAndSendVote(
-      {choice},
-      adapterName,
-      proposalHash
-    );
-
-    console.log('signature', signature);
   }
 
   return (
-    <VotingActionButtons
-      onClick={handleSubmitVote}
-      buttonProps={
-        {
-          // disabled: true,
-          // 'aria-disabled': true,
-        }
-      }
-      // voteChosen={}
-      // voteProgress={}
-    />
+    <>
+      <VotingActionButtons
+        onClick={isSubmitDisabled ? () => {} : handleSubmitVote}
+        buttonProps={{
+          disabled: isSubmitDisabled,
+          'aria-disabled': isSubmitDisabled,
+        }}
+        voteChosen={voteChosen}
+        voteProgress={voteChoiceProgress}
+      />
+
+      <ErrorMessageWithDetails
+        error={submitError}
+        renderText="Something went wrong"
+      />
+
+      {isDisabled && (
+        <button className="button--help" onClick={openWhyDisabledModal}>
+          Why is voting disabled?
+        </button>
+      )}
+
+      <WhyDisabledModal title="Why is voting disabled?" />
+    </>
   );
 }
