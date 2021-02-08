8de3d6f921a2cc34d49c67927d6c467f5d73f125
Jeremiah Naylor-Trein <jeremiahtrein@gmail.com>
2021-02-08 11:18:07 +0000

    updates ProposalActions to use new useProposalFlowStatus hook

--- src/components/proposals/ProposalActions.tsx
+++ src/components/proposals/ProposalActions.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 import {ContractAdapterNames} from '../web3/types';
 import {OffchainVotingStatus, OffchainVotingAction} from './voting';
-import {ProposalData} from './types';
-import {useVotingStartEnd} from './hooks/useVotingStartEnd';
+import {ProposalData, ProposalFlowStatus} from './types';
+import {useProposalFlowStatus} from './hooks/useProposalFlowStatus';
 import SponsorAction from './SponsorAction';
 
 type ProposalActionsProps = {
@@ -18,26 +18,33 @@ export default function ProposalActions(props: ProposalActionsProps) {
    * Our hooks
    */
 
-  const {hasVotingStarted, votingStartEndInitReady} = useVotingStartEnd(
-    proposal
-  );
+  const proposalStatus = useProposalFlowStatus(proposal);
 
   /**
    * Render
    */
 
+  if (!proposalStatus) return null;
+
   return (
     <div className="proposaldetails__button-container">
       {/* SPONSOR BUTTON */}
-      {votingStartEndInitReady && !hasVotingStarted && (
+      {proposalStatus === ProposalFlowStatus.Sponsor && (
         <SponsorAction proposal={proposal} />
       )}
 
-      {votingStartEndInitReady && hasVotingStarted && (
-        <>
-          <OffchainVotingStatus proposal={proposal} />
-          <OffchainVotingAction adapterName={adpaterName} proposal={proposal} />
-        </>
+      {/* OFF-CHAIN VOTING STATUS */}
+      {(proposalStatus === ProposalFlowStatus.OffchainVoting ||
+        proposalStatus === ProposalFlowStatus.OffchainVotingSubmitResult ||
+        proposalStatus === ProposalFlowStatus.OffchainVotingGracePeriod ||
+        proposalStatus === ProposalFlowStatus.Process ||
+        proposalStatus === ProposalFlowStatus.Completed) && (
+        <OffchainVotingStatus proposal={proposal} />
+      )}
+
+      {/* OFF-CHAIN VOTING BUTTONS */}
+      {proposalStatus === ProposalFlowStatus.OffchainVoting && (
+        <OffchainVotingAction adapterName={adpaterName} proposal={proposal} />
       )}
     </div>
   );
