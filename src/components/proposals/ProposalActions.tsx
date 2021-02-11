6ce9a571ad617536e556ead9da059041565b39b8
Jeremiah Naylor-Trein <jeremiahtrein@gmail.com>
2021-02-11 17:17:14 +0000

    moves offchain rollup status in ProposalActions - maybe should put it elsewhere...

--- src/components/proposals/ProposalActions.tsx
+++ src/components/proposals/ProposalActions.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 
 import {ContractAdapterNames} from '../web3/types';
+import {OffchainOpRollupVotingSubmitResultAction} from './voting';
 import {OffchainVotingStatus, OffchainVotingAction} from './voting';
-import {OffchainVotingSubmitResultAction} from './voting/OffchainVotingSubmitResultAction';
 import {ProposalData, ProposalFlowStatus} from './types';
 import {useProposalFlowStatus} from './hooks/useProposalFlowStatus';
 import SponsorAction from './SponsorAction';
+import {useWeb3Modal} from '../web3/hooks';
 
 type ProposalActionsProps = {
   adpaterName: ContractAdapterNames;
@@ -29,12 +30,8 @@ export default function ProposalActions(props: ProposalActionsProps) {
 
   return (
     <div className="proposaldetails__button-container">
-      {/* SPONSOR BUTTON */}
-      {proposalStatus === ProposalFlowStatus.Sponsor && (
-        <SponsorAction proposal={proposal} />
-      )}
-
       {/* OFF-CHAIN VOTING STATUS */}
+      {/* @todo Check which type of voting is enabled */}
       {(proposalStatus === ProposalFlowStatus.OffchainVoting ||
         proposalStatus === ProposalFlowStatus.OffchainVotingSubmitResult ||
         proposalStatus === ProposalFlowStatus.OffchainVotingGracePeriod ||
@@ -43,14 +40,20 @@ export default function ProposalActions(props: ProposalActionsProps) {
         <OffchainVotingStatus proposal={proposal} />
       )}
 
+      {/* SPONSOR BUTTON */}
+      {proposalStatus === ProposalFlowStatus.Sponsor && (
+        <SponsorAction adapterName={adpaterName} proposal={proposal} />
+      )}
+
       {/* OFF-CHAIN VOTING BUTTONS */}
       {proposalStatus === ProposalFlowStatus.OffchainVoting && (
         <OffchainVotingAction adapterName={adpaterName} proposal={proposal} />
       )}
 
-      {/* SUBMIT VOTE RESULT */}
+      {/* OFF-CHAIN VOTING (OPT. ROLLUP) SUBMIT VOTE RESULT */}
+      {/* @todo Perhaps a wrapping component to get the correct off-chain voting component */}
       {proposalStatus === ProposalFlowStatus.OffchainVotingSubmitResult && (
-        <OffchainVotingSubmitResultAction
+        <OffchainOpRollupVotingSubmitResultAction
           adapterName={adpaterName}
           proposal={proposal}
         />
