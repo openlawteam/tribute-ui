2323017b8abb24eddedc3df49df4cd5ea3040c93
Jeremiah Naylor-Trein <jeremiahtrein@gmail.com>
2021-02-12 11:22:09 +0000

    updates svg prop type for CheckSVG and StopwatchSVG

--- src/assets/svg/CheckSVG.tsx
+++ src/assets/svg/CheckSVG.tsx
@@ -1,4 +1,6 @@
-export default function CheckSVG(props: any) {
+import {SVGAttributes} from 'react';
+
+export default function CheckSVG(props: SVGAttributes<HTMLOrSVGElement>) {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
