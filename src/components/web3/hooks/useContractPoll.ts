07ce48b2ba394b319b5d6c1bf8abd694b476f8ad
Jeremiah Naylor-Trein <jeremiahtrein@gmail.com>
2021-02-15 16:21:27 +0000

    adds useContractPoll hook and unit tests

---
+++ src/components/web3/hooks/useContractPoll.ts
@@ -0,0 +1,113 @@
+import {useCallback, useEffect, useRef, useState} from 'react';
+import {Contract} from 'web3-eth-contract/types';
+
+type UseContractPollOptions = {
+  initialCallBeforeWait?: boolean;
+  pollInterval?: number;
+};
+
+type UseContractPollReturn<T> = {
+  pollContract: (data: PollContractArgs) => void;
+  pollContractData: T | undefined;
+  pollContractError: Error | undefined;
+  stopPollingContract: () => void;
+};
+
+type PollContractArgs = {
+  methodName: string;
+  contractInstanceMethods: typeof Contract.prototype.methods;
+  methodArguments: any[];
+};
+
+export function useContractPoll<T>(
+  options?: UseContractPollOptions
+): UseContractPollReturn<T> {
+  const {
+    initialCallBeforeWait = true,
+    pollInterval = 15000 /* 15 sec default */,
+  } = options || {};
+
+  /**
+   * State
+   */
+
+  const [pollContractData, setPollContractData] = useState<T>();
+  const [pollContractError, setPollContractError] = useState<Error>();
+
+  /**
+   * Cached callbacks
+   */
+
+  const callContractCached = useCallback(callContract, []);
+  const pollContractCached = useCallback(pollContract, [
+    callContractCached,
+    initialCallBeforeWait,
+    pollInterval,
+  ]);
+  const stopPollingContractCached = useCallback(stopPollingContract, []);
+
+  /**
+   * Refs
+   */
+
+  const intervalIdRef = useRef<NodeJS.Timeout>();
+
+  /**
+   * Effects
+   */
+
+  useEffect(() => {
+    // Clean up on unmount
+    return () => {
+      intervalIdRef.current && clearInterval(intervalIdRef.current);
+    };
+  }, []);
+
+  /**
+   * Functions
+   */
+
+  async function callContract({
+    methodArguments,
+    methodName,
+    contractInstanceMethods,
+  }: PollContractArgs): Promise<T> {
+    try {
+      const method = contractInstanceMethods[methodName];
+
+      return await method(...methodArguments).call();
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  function pollContract(data: PollContractArgs): void {
+    try {
+      intervalIdRef.current && clearInterval(intervalIdRef.current);
+
+      const intervalFunction = async () => {
+        const responseData = await callContractCached(data);
+        setPollContractData(responseData);
+      };
+
+      initialCallBeforeWait && intervalFunction();
+
+      const intervalIdToSet = setInterval(intervalFunction, pollInterval);
+
+      intervalIdRef.current = intervalIdToSet;
+    } catch (error) {
+      setPollContractError(error);
+    }
+  }
+
+  function stopPollingContract() {
+    intervalIdRef.current && clearInterval(intervalIdRef.current);
+  }
+
+  return {
+    pollContract: pollContractCached,
+    pollContractData,
+    pollContractError,
+    stopPollingContract: stopPollingContractCached,
+  };
+}
---
+++ src/components/web3/hooks/useContractPoll.unit.test.ts
@@ -0,0 +1,281 @@
+import {AbiItem} from 'web3-utils/types';
+import {renderHook, act} from '@testing-library/react-hooks';
+import {waitFor} from '@testing-library/react';
+
+import {DAO_REGISTRY_CONTRACT_ADDRESS, DEFAULT_CHAIN} from '../../../config';
+import {DEFAULT_ETH_ADDRESS, getWeb3Instance} from '../../../test/helpers';
+import {useContractPoll} from './useContractPoll';
+import DAORegistryABI from '../../../truffle-contracts/DaoRegistry.json';
+
+describe('useContractPoll unit tests', () => {
+  test('should poll contract', async () => {
+    await act(async () => {
+      const {result} = await renderHook(() =>
+        useContractPoll({pollInterval: 200})
+      );
+
+      const {web3, mockWeb3Provider} = getWeb3Instance();
+      const contractAddress = DAO_REGISTRY_CONTRACT_ADDRESS[DEFAULT_CHAIN];
+      const instance = new web3.eth.Contract(
+        DAORegistryABI as AbiItem[],
+        contractAddress
+      );
+
+      const injectResults: [string, string, string] = [
+        web3.eth.abi.encodeParameter(
+          {
+            Proposal: {
+              adapterAddress: 'address',
+              flags: 'uint256',
+            },
+          },
+          {adapterAddress: DEFAULT_ETH_ADDRESS, flags: 1}
+        ),
+        web3.eth.abi.encodeParameter(
+          {
+            Proposal: {
+              adapterAddress: 'address',
+              flags: 'uint256',
+            },
+          },
+          {adapterAddress: DEFAULT_ETH_ADDRESS, flags: 3}
+        ),
+        web3.eth.abi.encodeParameter(
+          {
+            Proposal: {
+              adapterAddress: 'address',
+              flags: 'uint256',
+            },
+          },
+          {adapterAddress: DEFAULT_ETH_ADDRESS, flags: 7}
+        ),
+      ];
+
+      // Proposal exists
+      mockWeb3Provider.injectResult(injectResults[0]);
+      // Proposal sponsored
+      mockWeb3Provider.injectResult(injectResults[1]);
+      // Proposal processed
+      mockWeb3Provider.injectResult(injectResults[2]);
+
+      result.current.pollContract({
+        methodArguments: [
+          '0x5a9d036a3844896bb2285162165828356b17584d6d5f51f4cbfa91dd4187ae34',
+        ],
+        methodName: 'proposals',
+        contractInstanceMethods: instance.methods,
+      });
+
+      await new Promise((r) => setTimeout(r, 0));
+
+      // Assert initial result before first poll to be `injectResults[0]`
+      expect(result.current.pollContractData).toEqual({
+        '0': '0x0000000000000000000000000000000000000000',
+        '1': '1',
+        adapterAddress: '0x0000000000000000000000000000000000000000',
+        flags: '1',
+      });
+
+      await new Promise((r) => setTimeout(r, 250));
+
+      expect(result.current.pollContractData).toEqual({
+        '0': '0x0000000000000000000000000000000000000000',
+        '1': '3',
+        adapterAddress: '0x0000000000000000000000000000000000000000',
+        flags: '3',
+      });
+
+      await new Promise((r) => setTimeout(r, 250));
+
+      expect(result.current.pollContractData).toEqual({
+        '0': '0x0000000000000000000000000000000000000000',
+        '1': '7',
+        adapterAddress: '0x0000000000000000000000000000000000000000',
+        flags: '7',
+      });
+    });
+  });
+
+  test('should stop polling contract when called', async () => {
+    await act(async () => {
+      const {result} = await renderHook(() =>
+        useContractPoll({pollInterval: 200})
+      );
+
+      const {web3, mockWeb3Provider} = getWeb3Instance();
+      const contractAddress = DAO_REGISTRY_CONTRACT_ADDRESS[DEFAULT_CHAIN];
+      const instance = new web3.eth.Contract(
+        DAORegistryABI as AbiItem[],
+        contractAddress
+      );
+
+      const injectResults: [string, string, string] = [
+        web3.eth.abi.encodeParameter(
+          {
+            Proposal: {
+              adapterAddress: 'address',
+              flags: 'uint256',
+            },
+          },
+          {adapterAddress: DEFAULT_ETH_ADDRESS, flags: 1}
+        ),
+        web3.eth.abi.encodeParameter(
+          {
+            Proposal: {
+              adapterAddress: 'address',
+              flags: 'uint256',
+            },
+          },
+          {adapterAddress: DEFAULT_ETH_ADDRESS, flags: 3}
+        ),
+        web3.eth.abi.encodeParameter(
+          {
+            Proposal: {
+              adapterAddress: 'address',
+              flags: 'uint256',
+            },
+          },
+          {adapterAddress: DEFAULT_ETH_ADDRESS, flags: 7}
+        ),
+      ];
+
+      // Proposal exists
+      mockWeb3Provider.injectResult(injectResults[0]);
+      // Proposal sponsored
+      mockWeb3Provider.injectResult(injectResults[1]);
+      // Proposal processed
+      mockWeb3Provider.injectResult(injectResults[2]);
+
+      result.current.pollContract({
+        methodArguments: [
+          '0x5a9d036a3844896bb2285162165828356b17584d6d5f51f4cbfa91dd4187ae34',
+        ],
+        methodName: 'proposals',
+        contractInstanceMethods: instance.methods,
+      });
+
+      await waitFor(() => {
+        expect(result.current.pollContractData).toEqual({
+          '0': '0x0000000000000000000000000000000000000000',
+          '1': '1',
+          adapterAddress: '0x0000000000000000000000000000000000000000',
+          flags: '1',
+        });
+      });
+
+      await new Promise((r) => setTimeout(r, 250));
+
+      expect(result.current.pollContractData).toEqual({
+        '0': '0x0000000000000000000000000000000000000000',
+        '1': '3',
+        adapterAddress: '0x0000000000000000000000000000000000000000',
+        flags: '3',
+      });
+
+      // Stop polling
+      result.current.stopPollingContract();
+
+      await new Promise((r) => setTimeout(r, 250));
+
+      // Assert result is same as last poll result, not `injectResults[2]`
+      expect(result.current.pollContractData).toEqual({
+        '0': '0x0000000000000000000000000000000000000000',
+        '1': '3',
+        adapterAddress: '0x0000000000000000000000000000000000000000',
+        flags: '3',
+      });
+    });
+  });
+
+  test('should not immediately call contract', async () => {
+    await act(async () => {
+      const {result} = await renderHook(() =>
+        useContractPoll({pollInterval: 200, initialCallBeforeWait: false})
+      );
+
+      const {web3, mockWeb3Provider} = getWeb3Instance();
+      const contractAddress = DAO_REGISTRY_CONTRACT_ADDRESS[DEFAULT_CHAIN];
+      const instance = new web3.eth.Contract(
+        DAORegistryABI as AbiItem[],
+        contractAddress
+      );
+
+      const injectResults: [string, string, string] = [
+        web3.eth.abi.encodeParameter(
+          {
+            Proposal: {
+              adapterAddress: 'address',
+              flags: 'uint256',
+            },
+          },
+          {adapterAddress: DEFAULT_ETH_ADDRESS, flags: 1}
+        ),
+        web3.eth.abi.encodeParameter(
+          {
+            Proposal: {
+              adapterAddress: 'address',
+              flags: 'uint256',
+            },
+          },
+          {adapterAddress: DEFAULT_ETH_ADDRESS, flags: 3}
+        ),
+        web3.eth.abi.encodeParameter(
+          {
+            Proposal: {
+              adapterAddress: 'address',
+              flags: 'uint256',
+            },
+          },
+          {adapterAddress: DEFAULT_ETH_ADDRESS, flags: 7}
+        ),
+      ];
+
+      // Proposal exists
+      mockWeb3Provider.injectResult(injectResults[0]);
+      // Proposal sponsored
+      mockWeb3Provider.injectResult(injectResults[1]);
+      // Proposal processed
+      mockWeb3Provider.injectResult(injectResults[2]);
+
+      result.current.pollContract({
+        methodArguments: [
+          '0x5a9d036a3844896bb2285162165828356b17584d6d5f51f4cbfa91dd4187ae34',
+        ],
+        methodName: 'proposals',
+        contractInstanceMethods: instance.methods,
+      });
+
+      await new Promise((r) => setTimeout(r, 0));
+
+      // Assert initial result before first poll to be `undefined`
+      expect(result.current.pollContractData).toBe(undefined);
+
+      await new Promise((r) => setTimeout(r, 250));
+
+      expect(result.current.pollContractData).toEqual({
+        '0': '0x0000000000000000000000000000000000000000',
+        '1': '1',
+        adapterAddress: '0x0000000000000000000000000000000000000000',
+        flags: '1',
+      });
+
+      await new Promise((r) => setTimeout(r, 250));
+
+      expect(result.current.pollContractData).toEqual({
+        '0': '0x0000000000000000000000000000000000000000',
+        '1': '3',
+        adapterAddress: '0x0000000000000000000000000000000000000000',
+        flags: '3',
+      });
+
+      await new Promise((r) => setTimeout(r, 250));
+
+      expect(result.current.pollContractData).toEqual({
+        '0': '0x0000000000000000000000000000000000000000',
+        '1': '7',
+        adapterAddress: '0x0000000000000000000000000000000000000000',
+        flags: '7',
+      });
+    });
+  });
+});
