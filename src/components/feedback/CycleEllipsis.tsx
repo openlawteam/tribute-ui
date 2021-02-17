fe873968143c4fb733ab0dec8af414fa693b4536
Jeremiah Naylor-Trein <jeremiahtrein@gmail.com>
2021-02-17 15:49:51 +0000

    CycleEllipsis component improvements

--- src/components/feedback/CycleEllipsis.tsx
+++ src/components/feedback/CycleEllipsis.tsx
@@ -6,15 +6,24 @@ type CycleEllipsisProps = {
    * What is the interval of cycling the message?
    */
   intervalMs?: number;
+  /**
+   * Optional settings for the inner `<FadeIn />` component.
+   */
+  fadeInProps?: Parameters<typeof FadeIn>[0];
 };
 
 const MESSAGES = ['', '.', '.', '.'];
 const rootStyles = {position: 'absolute'} as CSSProperties;
+const nbspStyles = {
+  height: 1,
+  width: 1,
+} as CSSProperties;
 
 export function CycleEllipsis(props: CycleEllipsisProps) {
-  const {intervalMs = 1000} = props;
+  const {intervalMs = 1000, fadeInProps} = props;
+  const fadeInPropsMerged = {...fadeInProps, inline: true};
 
-  const [upToIndex, setUpToIndex] = useState<number>(0);
+  const [upToIndex, setUpToIndex] = useState<number>(1);
 
   useEffect(() => {
     const intervalId = setInterval(
@@ -28,11 +37,27 @@ export function CycleEllipsis(props: CycleEllipsisProps) {
   }, [intervalMs]);
 
   return (
-    <span style={rootStyles}>
-      <span>{upToIndex >= 0 && MESSAGES[0]}</span>
-      <span>{upToIndex >= 1 && <FadeIn inline>{MESSAGES[1]}</FadeIn>}</span>
-      <span>{upToIndex >= 2 && <FadeIn inline>{MESSAGES[2]}</FadeIn>}</span>
-      <span>{upToIndex >= 3 && <FadeIn inline>{MESSAGES[3]}</FadeIn>}</span>
-    </span>
+    <>
+      <span style={rootStyles}>
+        <span>{upToIndex >= 0 && MESSAGES[0]}</span>
+        <span>
+          {upToIndex >= 1 && (
+            <FadeIn {...fadeInPropsMerged}>{MESSAGES[1]}</FadeIn>
+          )}
+        </span>
+        <span>
+          {upToIndex >= 2 && (
+            <FadeIn {...fadeInPropsMerged}>{MESSAGES[2]}</FadeIn>
+          )}
+        </span>
+        <span>
+          {upToIndex >= 3 && (
+            <FadeIn {...fadeInPropsMerged}>{MESSAGES[3]}</FadeIn>
+          )}
+        </span>
+      </span>
+      {/* @note Keep as it's an alignment fallback in certain CSS situations when no other text is a sibiling */}
+      <span style={nbspStyles}>&nbsp;</span>
+    </>
   );
 }
