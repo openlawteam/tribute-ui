02823e8a085e9b783e598b0bf1d1f7aa83c7f471
Jeremiah Naylor-Trein <jeremiahtrein@gmail.com>
2021-03-03 17:27:24 +0000

    adds helper signTypedDataV4 web3 mock response

---
+++ src/test/web3Responses/signTypedDataV4.ts
@@ -0,0 +1,20 @@
+import {TestWeb3ResponseArgs, TestWeb3ResponseReturn} from './types';
+
+/**
+ * signTypedDataV4
+ *
+ * @param {TestWeb3ResponseArgs}
+ * @returns {TestWeb3ResponseReturn<string>}
+ * @see https://docs.metamask.io/guide/signing-data.html#sign-typed-data-v4
+ */
+export const signTypedDataV4 = ({
+  result,
+  web3Instance,
+}: TestWeb3ResponseArgs): TestWeb3ResponseReturn<string> => [
+  result ??
+    web3Instance.eth.abi.encodeParameter(
+      'bytes32',
+      web3Instance.utils.toHex('great signature')
+    ),
+  {rpcMethodName: 'eth_signTypedData_v4'},
+];
