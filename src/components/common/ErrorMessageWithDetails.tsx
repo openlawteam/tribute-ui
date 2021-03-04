c93a2ea1a5a607e70ddc0bda95bee4e56c2cb790
Jeremiah Naylor-Trein <jeremiahtrein@gmail.com>
2021-03-04 11:57:48 +0000

    Improves user rejected request wallet tx error (#165)
    * improves scope of detection of a user rejected request error from wallet
    
    * only render status div if logic passes

--- src/components/common/ErrorMessageWithDetails.tsx
+++ src/components/common/ErrorMessageWithDetails.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import {normalizeString} from '../../util/helpers';
 
 import FadeIn from './FadeIn';
 
@@ -13,8 +14,15 @@ export default function ErrorMessageWithDetails(
 ) {
   const {error, renderText} = props;
 
+  // @link https://github.com/ethereum/EIPs/blob/master/EIPS/eip-1193.md#provider-errors
+  const isWalletRejectedRequest =
+    (error as any)?.code === 4001 ||
+    /^(the )?user rejected (the )?request$/g.test(
+      normalizeString(error?.message || '')
+    );
+
   // @note Some wallets will provide proper error codes. The `4001` is a "user rejected transaction".
-  return error && (error as any)?.code !== 4001 ? (
+  return error && !isWalletRejectedRequest ? (
     <FadeIn>
       <div className="text-center">
         <p className="error-message">
