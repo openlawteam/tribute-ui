90ec1b7d4227402fdb7b3c62a62252e6fcb4de48
Jeremiah Naylor-Trein <jeremiahtrein@gmail.com>
2021-03-05 13:11:40 +0000

    adds accessible aria-label to LoaderWithEmoji

--- src/components/feedback/LoaderWithEmoji.tsx
+++ src/components/feedback/LoaderWithEmoji.tsx
@@ -1,7 +1,7 @@
 export default function LoaderWithEmoji() {
   return (
     <div className="loader--emoji">
-      <span role="img" aria-label="hourglass emoji">
+      <span role="img" aria-label="Loading content...">
         ⏳
       </span>
     </div>
