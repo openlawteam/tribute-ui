0966710a7abe2a12443f9d44ee20ede6646d6bf0
Jeremiah Naylor-Trein <jeremiahtrein@gmail.com>
2021-03-15 15:12:54 +0000

    include Proposals inside the Membership listing

--- src/pages/membership/Membership.tsx
+++ src/pages/membership/Membership.tsx
@@ -1,21 +1,10 @@
 import React from 'react';
 import {useHistory} from 'react-router-dom';
 
-import {ProposalHeaderNames} from '../../util/enums';
-import {truncateEthAddress} from '../../util/helpers';
-import Wrap from '../../components/common/Wrap';
-import FadeIn from '../../components/common/FadeIn';
-import {useProposals} from '../../components/proposals/hooks/useProposals';
 import {DaoConstants} from '../../components/adapters-extensions/enums';
-import ProposalCard from '../../components/proposals/ProposalCard';
+import FadeIn from '../../components/common/FadeIn';
 import Proposals from '../../components/proposals/Proposals';
-// import {
-//   fakeMembershipProposalsVoting,
-//   fakeMembershipProposalsRequest,
-//   fakeMembershipProposalsPassed,
-//   fakeMembershipProposalsFailed,
-//   FakeProposal,
-// } from '../../components/proposals/_mockData';
+import Wrap from '../../components/common/Wrap';
 
 export default function Membership() {
   /**
@@ -25,114 +14,25 @@ export default function Membership() {
   const history = useHistory();
 
   /**
-   * Our hooks
-   */
-
-  const {proposals, proposalsStatus} = useProposals({
-    adapterName: DaoConstants.ONBOARDING,
-  });
-
-  /**
-   * Variables
-   */
-
-  const votingProposals = /* renderProposalCards(fakeMembershipProposalsVoting) */ [] as any;
-  const requestProposals = /* renderProposalCards(fakeMembershipProposalsRequest) */ [] as any;
-  const passedProposals = /* renderProposalCards(fakeMembershipProposalsPassed) */ [] as any;
-  const failedProposals = /* renderProposalCards(fakeMembershipProposalsFailed) */ [] as any;
-
-  /**
    * Functions
    */
 
-  // @note TEST
-  return (
-    <RenderWrapper>
-      <Proposals proposals={proposals} />
-
-      {proposals.map((proposal) => {
-        return (
-          <ProposalCard
-            key={
-              proposal.snapshotDraft?.idInDAO ||
-              proposal.snapshotProposal?.idInDAO
-            }
-            onClick={(id) => history.push(`membership/${id}`)}
-            proposal={proposal}
-            name={truncateEthAddress(
-              proposal.snapshotProposal?.msg.payload.name ||
-                proposal.snapshotDraft?.msg.payload.name ||
-                '',
-              7
-            )}
-          />
-        );
-      })}
-    </RenderWrapper>
-  );
-
-  /* function renderProposalCards(proposals: FakeProposal[]) {
-    return proposals.map((proposal) => {
-      return (
-        <ProposalCard
-          key={proposal.snapshotProposal.hash}
-          onClick={handleClickProposalDetails(proposal.snapshotProposal.hash)}
-          proposal={proposal}
-          name={truncateEthAddress(proposal.snapshotProposal.name, 7)}
-        />
-      );
-    });
-  } */
-
-  /* function handleClickProposalDetails(proposalHash: string) {
-    return () => {
-      if (!proposalHash) return;
-
-      history.push(`/membership/${proposalHash}`);
-    };
-  } */
+  function handleClickProposalDetails(proposalHash: string) {
+    proposalHash && history.push(`/membership/${proposalHash}`);
+  }
 
   /**
    * Render
    */
 
-  // return (
-  //   <RenderWrapper>
-  //     <div className="grid--fluid grid-container">
-  //       {/* VOTING PROPOSALS */}
-  //       {votingProposals.length > 0 && (
-  //         <>
-  //           <div className="grid__header">{ProposalHeaderNames.VOTING}</div>
-  //           <div className="grid__cards">{votingProposals}</div>
-  //         </>
-  //       )}
-
-  //       {/* PENDING PROPOSALS (DRAFTS, NOT SPONSORED) */}
-  //       {requestProposals.length > 0 && (
-  //         <>
-  //           <div className="grid__header">{ProposalHeaderNames.REQUESTS}</div>
-  //           <div className="grid__cards">{requestProposals}</div>
-  //         </>
-  //       )}
-
-  //       {/* PASSED PROPOSALS */}
-  //       {passedProposals.length > 0 && (
-  //         <>
-  //           <div className="grid__header">{ProposalHeaderNames.PASSED}</div>
-  //           <div className="grid__cards">{passedProposals}</div>
-  //         </>
-  //       )}
-
-  //       {/* FAILED PROPOSALS */}
-  //       {failedProposals.length > 0 && (
-  //         <>
-  //           <div className="grid__header">{ProposalHeaderNames.FAILED}</div>
-  //           <div className="grid__cards">{failedProposals}</div>
-  //         </>
-  //       )}
-  //     </div>
-  //   </RenderWrapper>
-  // );
+  return (
+    <RenderWrapper>
+      <Proposals
+        adapterName={DaoConstants.ONBOARDING}
+        onProposalClick={handleClickProposalDetails}
+      />
+    </RenderWrapper>
+  );
 }
 
 function RenderWrapper(props: React.PropsWithChildren<any>): JSX.Element {
@@ -164,8 +64,8 @@ function RenderWrapper(props: React.PropsWithChildren<any>): JSX.Element {
             View members
           </button>
         </div>
+
         {/* RENDER CHILDREN */}
-        @TODO
         {props.children}
       </FadeIn>
     </Wrap>
