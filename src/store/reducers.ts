85512cf8494228cce744d99ea3dad64266cef70e
Jeremiah Naylor-Trein <jeremiahtrein@gmail.com>
2021-03-31 17:58:40 +0100

    adds Redux state for subgraphNetworkStatus

--- src/store/reducers.ts
+++ src/store/reducers.ts
@@ -2,8 +2,10 @@ import {combineReducers} from 'redux';
 
 import connectedMember from './connectedMember/reducers';
 import contracts from './contracts/reducers';
+import subgraphNetworkStatus from './subgraphNetworkStatus/reducers';
 
 export default combineReducers({
   connectedMember,
   contracts,
+  subgraphNetworkStatus,
 });
