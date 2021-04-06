2721e7cb981eb9ebcdb0cc82a70b2c87302673d3
Jeremiah Naylor-Trein <jeremiahtrein@gmail.com>
2021-04-06 11:45:51 +0100

    simplifies Web3ModalManager unit test for default web3 instance, provider

--- src/components/web3/Web3ModalManager.unit.test.tsx
+++ src/components/web3/Web3ModalManager.unit.test.tsx
@@ -1,6 +1,11 @@
 import {render, waitFor} from '@testing-library/react';
 
-import Web3ModalManager, {Web3ModalContext} from './Web3ModalManager';
+import Web3ModalManager, {
+  Web3ModalContext,
+  Web3ModalContextValue,
+} from './Web3ModalManager';
+import {rest, server} from '../../test/server';
+import Wrapper from '../../test/Wrapper';
 
 describe('Web3ModalManager unit tests', () => {
   const providerOptions = {
@@ -13,25 +18,38 @@ describe('Web3ModalManager unit tests', () => {
     },
   };
 
-  test('should render correct default context', async () => {
-    process.env.REACT_APP_DEFAULT_CHAIN_NAME_LOCAL = 'RINKEBY';
+  test('should provide default web3 instance and provider', async () => {
+    let web3Context: Web3ModalContextValue;
+
+    /**
+     * Mock the websocket connection to the Web3 instance.
+     * Not sure why the test `server` intercepts it as http(s),
+     * but this seems to make it happy.
+     */
+    server.use(
+      rest.get('http://127.0.0.1:7545/', async (_req, res, ctx) =>
+        res(ctx.status(200))
+      )
+    );
 
     render(
-      <Web3ModalManager providerOptions={providerOptions}>
-        <></>
-      </Web3ModalManager>
+      <Wrapper>
+        <Web3ModalManager providerOptions={providerOptions}>
+          <Web3ModalContext.Consumer>
+            {(context) => {
+              web3Context = context;
+
+              return <></>;
+            }}
+          </Web3ModalContext.Consumer>
+        </Web3ModalManager>
+      </Wrapper>
     );
 
     await waitFor(() => {
-      <Web3ModalContext.Consumer>
-        {({web3Instance, provider, networkId}) => {
-          expect(web3Instance).toBeDefined();
-          expect(provider).toBeDefined();
-          expect(networkId).toBe(0);
-
-          return <></>;
-        }}
-      </Web3ModalContext.Consumer>;
+      expect(web3Context.web3Instance).toBeDefined();
+      expect(web3Context.provider).toBeDefined();
+      expect(web3Context.networkId).toBeUndefined();
     });
   });
 });
