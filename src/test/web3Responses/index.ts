9c78c3983505eb3a5a23b4e9cdc6bd9dd949249f
Jeremiah Naylor-Trein <jeremiahtrein@gmail.com>
2021-04-13 18:12:59 +0100

    removes unneeded test/web3Responses/isActiveMember

--- src/test/web3Responses/index.ts
+++ src/test/web3Responses/index.ts
@@ -3,7 +3,6 @@ export * from './ethBlockNumber';
 export * from './gasPrice';
 export * from './getCurrentDelegateKey';
 export * from './getTransactionReceipt';
-export * from './isActiveMember';
 export * from './member';
 export * from './sendTransaction';
 export * from './signTypedDataV4';
