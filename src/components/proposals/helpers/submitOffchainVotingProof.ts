573a7c19c4f212ca93949ad293e2d3957449bda6
Jeremiah Naylor-Trein <jeremiahtrein@gmail.com>
2021-05-05 14:33:31 +0100

    refactors submitOffchainVotingProof into a helper from a hook

---
+++ src/components/proposals/helpers/submitOffchainVotingProof.ts
@@ -0,0 +1,52 @@
+import {SNAPSHOT_HUB_API_URL, SPACE} from '../../../config';
+
+type SubmitOffchainVotingProofArguments = {
+  /**
+   * DAO Adapter address
+   */
+  actionId: string;
+  /**
+   * Blockchain network ID
+   */
+  chainId: number;
+  /**
+   * Hex string of the Merkle root
+   */
+  merkleRoot: string;
+  /**
+   * An array of Merkle tree steps
+   */
+  steps: any[];
+  /**
+   * DAO Registry address
+   */
+  verifyingContract: string;
+};
+
+/**
+ * submitOffchainVotingProof
+ *
+ * Submits a Merkle hex root and its steps to Snapshot Hub
+ * for verification and storage (if successful).
+ *
+ * @link https://github.com/openlawteam/snapshot-hub
+ */
+export async function submitOffchainVotingProof(
+  data: SubmitOffchainVotingProofArguments
+): Promise<void> {
+  try {
+    // Endpoint does not have any return data.
+    const response = await fetch(
+      `${SNAPSHOT_HUB_API_URL}/api/${SPACE}/offchain_proofs`,
+      {method: 'POST', body: JSON.stringify(data)}
+    );
+
+    if (!response.ok) {
+      throw new Error(
+        'Something went wrong while submitting the off-chain vote proof.'
+      );
+    }
+  } catch (error) {
+    throw error;
+  }
+}
---
+++ src/components/proposals/helpers/submitOffchainVotingProof.unit.test.ts
@@ -0,0 +1,87 @@
+import {rest, server} from '../../../test/server';
+import {SNAPSHOT_HUB_API_URL, SPACE} from '../../../config';
+import {submitOffchainVotingProof} from './submitOffchainVotingProof';
+
+describe('submitOffchainVotingProof unit tests', () => {
+  test('can call `submitOffchainVotingProof` and return OK', async () => {
+    server.use(
+      rest.post(
+        `${SNAPSHOT_HUB_API_URL}/api/${SPACE}/offchain_proofs`,
+        (_req, res, ctx) => res(ctx.status(201))
+      )
+    );
+
+    let testError: any;
+
+    try {
+      // Using fake data
+      await submitOffchainVotingProof({
+        verifyingContract: '',
+        actionId: '',
+        merkleRoot: '',
+        steps: [],
+        chainId: 1337,
+      });
+    } catch (error) {
+      testError = error;
+    }
+
+    expect(testError).toBe(undefined);
+  });
+
+  test('can throw error when server error', async () => {
+    server.use(
+      rest.post(
+        `${SNAPSHOT_HUB_API_URL}/api/${SPACE}/offchain_proofs`,
+        (_req, res, ctx) => res(ctx.status(500))
+      )
+    );
+
+    let testError: any;
+
+    try {
+      // Using fake data
+      await submitOffchainVotingProof({
+        verifyingContract: '',
+        actionId: '',
+        merkleRoot: '',
+        steps: [],
+        chainId: 1337,
+      });
+    } catch (error) {
+      testError = error;
+    }
+
+    expect(testError?.message).toMatch(
+      /something went wrong while submitting the off-chain vote proof\./i
+    );
+  });
+
+  test('can throw error when client error', async () => {
+    server.use(
+      rest.post(
+        `${SNAPSHOT_HUB_API_URL}/api/${SPACE}/offchain_proofs`,
+        (_req, res, ctx) => res(ctx.status(400))
+      )
+    );
+
+    let testError: any;
+
+    try {
+      // Using fake data
+      await submitOffchainVotingProof({
+        verifyingContract: '',
+        actionId: '',
+        merkleRoot: '',
+        steps: [],
+        chainId: 1337,
+      });
+    } catch (error) {
+      testError = error;
+    }
+
+    expect(testError?.message).toMatch(
+      /something went wrong while submitting the off-chain vote proof\./i
+    );
+  });
+});
