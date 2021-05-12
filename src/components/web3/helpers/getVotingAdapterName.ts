2eb4d41ee5e25912d3ccbe3addb6fd9a8f96ee15
Jeremiah Naylor-Trein <jeremiahtrein@gmail.com>
2021-05-12 17:17:40 +0100

    adds getVotingAdapterName helper and test

---
+++ src/components/web3/helpers/getVotingAdapterName.ts
@@ -0,0 +1,23 @@
+import {AbiItem} from 'web3-utils/types';
+import {VotingAdapterName} from '../../adapters-extensions/enums';
+import Web3 from 'web3';
+
+export async function getVotingAdapterName(
+  address: string,
+  web3Instance: Web3
+): Promise<VotingAdapterName> {
+  try {
+    const {default: lazyIVotingABI} = await import(
+      '../../../truffle-contracts/IVoting.json'
+    );
+
+    return await new web3Instance.eth.Contract(
+      lazyIVotingABI as AbiItem[],
+      address
+    ).methods
+      .getAdapterName()
+      .call();
+  } catch (error) {
+    throw error;
+  }
+}
