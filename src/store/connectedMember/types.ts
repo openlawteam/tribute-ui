9048c2fe0d40107ef4137ec66181b24abdd425bf
Jeremiah Naylor-Trein <jeremiahtrein@gmail.com>
2021-05-12 17:21:20 +0100

    updates connectedMember type

--- src/store/connectedMember/types.ts
+++ src/store/connectedMember/types.ts
@@ -1,5 +1,18 @@
 export type ConnectedMemberState = {
+  /**
+   * A member's account delegate address (will equal `memberAddress` if not delegated) in the DAO.
+   */
   delegateKey: string;
+  /**
+   * A member has chosen to delegate their address to another address in the DAO.
+   */
+  isAddressDelegated: boolean;
+  /**
+   * A member has `UNITS > 0` in the DAO.
+   */
   isActiveMember: boolean;
+  /**
+   * A member's true address in the DAO.
+   */
   memberAddress: string;
 } | null;
