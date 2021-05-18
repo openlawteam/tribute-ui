4ed6a88edf5d654b48d58bfe60a2e2e3b5d66628
sophiacodes <sophiacodes@gmail.com>
2021-05-18 17:26:05 +0100

    import web3-utils for isHex check

--- src/util/helpers/truncateSignature.ts
+++ src/util/helpers/truncateSignature.ts
@@ -1,7 +1,4 @@
-import Web3 from 'web3';
-
-export const isSignatureValid = (signature: string) =>
-  Web3.utils.isHex(signature);
+import {isHex} from 'web3-utils';
 
 /**
  * truncateSignature
@@ -15,7 +12,7 @@ export const isSignatureValid = (signature: string) =>
 export function truncateSignature(sig: string, maxLength: number = 5): string {
   if (sig === null || typeof sig === 'undefined') return '---';
 
-  if (isSignatureValid(sig)) {
+  if (isHex(sig)) {
     const firstSegment = sig.substring(0, maxLength);
     const secondPart = sig.substring(sig.length - 3);
     return firstSegment + '...' + secondPart;
