83c49604854a40579f41a0f8eb67cae19ada4280
jdville03 <jarrel.delottinville@gmail.com>
2021-06-24 16:20:07 -0700

    change DaoTokenHolder badge to check connected account against member address (not delegate address)

--- src/components/dao-token/DaoTokenHolder.tsx
+++ src/components/dao-token/DaoTokenHolder.tsx
@@ -62,7 +62,7 @@ export default function DaoTokenHolder(
 
       const holderData = holders?.find(
         (holder: any) =>
-          holder.member?.delegateKey.toLowerCase() === account.toLowerCase()
+          holder.member?.id.toLowerCase() === account.toLowerCase()
       );
 
       holderData &&
