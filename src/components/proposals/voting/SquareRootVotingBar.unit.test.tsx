65a6d068e3b012fc964900c2eab91e99cd1efba8
Jeremiah Naylor-Trein <jeremiahtrein@gmail.com>
2021-07-30 15:30:46 +0100

    voting weights are now two decimals (fixes #431) (#432)

--- src/components/proposals/voting/SquareRootVotingBar.unit.test.tsx
+++ src/components/proposals/voting/SquareRootVotingBar.unit.test.tsx
@@ -14,10 +14,27 @@ describe('SquareRootVotingBar unit tests', () => {
       />
     );
 
-    expect(screen.getByText('4%')).toBeInTheDocument();
-    expect(screen.getByText('2%')).toBeInTheDocument();
-    expect(screen.getByLabelText('4% yes votes')).toBeInTheDocument();
-    expect(screen.getByLabelText('2% no votes')).toBeInTheDocument();
+    expect(screen.getByText(/^4%$/i)).toBeInTheDocument();
+    expect(screen.getByText(/^2%$/i)).toBeInTheDocument();
+    expect(screen.getByLabelText(/^4% yes votes$/i)).toBeInTheDocument();
+    expect(screen.getByLabelText(/^2% no votes$/i)).toBeInTheDocument();
+  });
+
+  test('should show correct percentages to two decimal places', () => {
+    render(
+      <SquareRootVotingBar
+        yesUnits={400000}
+        noUnits={200000}
+        totalUnits={3000000}
+        votingExpired={true}
+        showPercentages={true}
+      />
+    );
+
+    expect(screen.getByText(/^13.33%$/i)).toBeInTheDocument();
+    expect(screen.getByText(/^6.67%$/i)).toBeInTheDocument();
+    expect(screen.getByLabelText(/^13.33% yes votes$/i)).toBeInTheDocument();
+    expect(screen.getByLabelText(/^6.67% no votes$/i)).toBeInTheDocument();
   });
 
   test('should not show percentages if `showPercentages` is false', () => {
@@ -31,10 +48,10 @@ describe('SquareRootVotingBar unit tests', () => {
       />
     );
 
-    expect(() => screen.getByText('4%')).toThrow();
-    expect(() => screen.getByText('2%')).toThrow();
-    expect(screen.getByLabelText('4% yes votes')).toBeInTheDocument();
-    expect(screen.getByLabelText('2% no votes')).toBeInTheDocument();
+    expect(() => screen.getByText(/^4%$/i)).toThrow();
+    expect(() => screen.getByText(/^2%$/i)).toThrow();
+    expect(screen.getByLabelText(/^4% yes votes$/i)).toBeInTheDocument();
+    expect(screen.getByLabelText(/^2% no votes$/i)).toBeInTheDocument();
   });
 
   test('no results should display if units values are not provided yet (i.e. async values)', () => {
@@ -48,8 +65,8 @@ describe('SquareRootVotingBar unit tests', () => {
       />
     );
 
-    expect(screen.getAllByText('0%')).toHaveLength(2);
-    expect(screen.getByLabelText('0% yes votes')).toBeInTheDocument();
-    expect(screen.getByLabelText('0% no votes')).toBeInTheDocument();
+    expect(screen.getAllByText(/^0%$/i)).toHaveLength(2);
+    expect(screen.getByLabelText(/^0% yes votes$/i)).toBeInTheDocument();
+    expect(screen.getByLabelText(/^0% no votes$/i)).toBeInTheDocument();
   });
 });
