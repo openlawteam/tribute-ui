f5e3472af06c1f03cf53dff71d3be939fed35905
Jeremiah Naylor-Trein <jeremiahtrein@gmail.com>
2021-12-07 15:10:30 +0000

    updatre `formatNumber` implementation

--- src/util/helpers/formatNumber.ts
+++ src/util/helpers/formatNumber.ts
@@ -3,14 +3,18 @@
  *
  * Formats a number (U.S. region) with commas (e.g. 1000 -> 1,000).
  *
+ * If an error occurs while converting to a `Number` the original value
+ * is attempted to be converted to a `string` and returned.
+ *
  * @param {string | number} value
  * @returns {string}
- *
- * @todo maybe a more friendly way via Intl API in JS core?
  */
 export const formatNumber = (value: number | string): string => {
-  const regEx = new RegExp(/(\d)(?=(\d{3})+(?:\.\d+)?$)/g);
-  return typeof value === 'number'
-    ? value.toString().replace(/,/g, '').replace(regEx, '$1,')
-    : value.replace(/,/g, '').replace(regEx, '$1,');
+  const number: number = Number(value ?? undefined);
+
+  if (isNaN(number)) {
+    return '';
+  }
+
+  return number.toLocaleString('en-US');
 };
