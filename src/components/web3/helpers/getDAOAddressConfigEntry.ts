149d8a67657134e7da248350a83452c7a6016984
jdville03 <jarrel.delottinville@gmail.com>
2022-01-14 14:01:20 -0800

    add helper to get DAO address config

---
+++ src/components/web3/helpers/getDAOAddressConfigEntry.ts
@@ -0,0 +1,27 @@
+import Web3 from 'web3';
+
+import {ContractDAOConfigKeys} from '../types';
+import {DaoRegistry} from '../../../../abi-types/DaoRegistry';
+
+export async function getDAOAddressConfigEntry(
+  configKey: ContractDAOConfigKeys,
+  daoContractInstance: DaoRegistry | undefined
+): Promise<string> {
+  try {
+    if (!daoContractInstance) {
+      throw new Error('No DaoRegistry contract instance provided.');
+    }
+
+    const configKeySha3 = Web3.utils.sha3(configKey);
+
+    if (!configKeySha3) {
+      throw new Error('No sha3 config key was returned.');
+    }
+
+    return await daoContractInstance.methods
+      .getAddressConfiguration(configKeySha3)
+      .call();
+  } catch (error) {
+    throw error;
+  }
+}
--- src/components/web3/helpers/index.ts
+++ src/components/web3/helpers/index.ts
@@ -3,6 +3,7 @@ export * from './contractSend';
 export * from './getAdapterAddress';
 export * from './getAdapterAddressFromContracts';
 export * from './getContractByAddress';
+export * from './getDAOAddressConfigEntry';
 export * from './getDAOConfigEntry';
 export * from './getDaoState';
 export * from './getExtensionAddress';
