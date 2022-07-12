84492075a69c03624a3d445282ff62a8104063bd
Marjorie N <marjorie.ngoupende@gmail.com>
2022-07-12 21:52:20 +0200

    fix.front/css page collection

--- src/pages/collection/Collection.tsx
+++ src/pages/collection/Collection.tsx
@@ -1,7 +1,145 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Button} from 'reactstrap';
+import {
+  CardGroup,
+  Col,
+  Row,
+  Card,
+  CardBody,
+  CardTitle,
+  CardSubtitle,
+  CardImg,
+  CardText,
+} from 'reactstrap';
+
 export default function Collection() {
   return (
-    <div>
-      <div className="interest_submit-proposal_xyz">
+    <div className="container">
+      <div className="row">
+        <h1>Collection</h1>
+      <p>
+        Below are works selected by members for ProdCapsule's permanent
+        collection.
+      </p>
+      </div>
+      <CardGroup>
+        <Card>
+          <CardImg
+            alt="Card image cap"
+            src="https://picsum.photos/318/180"
+            top
+            width="100%"
+          />
+          <CardBody>
+            <CardTitle tag="h5">Card title</CardTitle>
+            <CardSubtitle className="mb-2 text-muted" tag="h6">
+              NFT Title
+            </CardSubtitle>
+            <CardText  className="mb-2 text-muted">
+              Art Blocks
+            </CardText>
+            <Button>View details</Button>
+          </CardBody>
+        </Card>
+        <Card>
+          <CardImg
+            alt="Card image cap"
+            src="https://picsum.photos/318/180"
+            top
+            width="100%"
+          />
+          <CardBody>
+            <CardTitle tag="h5">Card title</CardTitle>
+            <CardSubtitle className="mb-2 text-muted" tag="h6">
+              NFT Title
+            </CardSubtitle>
+            <CardText  className="mb-2 text-muted">
+            Art Blocks
+            </CardText>
+            <Button>View details</Button>
+          </CardBody>
+        </Card>
+        <Card>
+          <CardImg
+            alt="Card image cap"
+            src="https://picsum.photos/318/180"
+            top
+            width="100%"
+          />
+          <CardBody>
+            <CardTitle tag="h5">Card title</CardTitle>
+            <CardSubtitle className="mb-2 text-muted" tag="h6">
+              NFT Title
+            </CardSubtitle>
+            <CardText  className="mb-2 text-muted">
+            Art Blocks
+            </CardText>
+            <Button>View details</Button>
+          </CardBody>
+        </Card>
+      </CardGroup>
+      <CardGroup>
+        <Card>
+          <CardImg
+            alt="Card image cap"
+            src="https://picsum.photos/318/180"
+            top
+            width="100%"
+          />
+          <CardBody>
+            <CardTitle tag="h5">Card title</CardTitle>
+            <CardSubtitle className="mb-2 text-muted" tag="h6">
+              NFT Title
+            </CardSubtitle>
+            <CardText  className="mb-2 text-muted">
+              Art Blocks
+            </CardText>
+            <Button>View details</Button>
+          </CardBody>
+        </Card>
+        <Card>
+          <CardImg
+            alt="Card image cap"
+            src="https://picsum.photos/318/180"
+            top
+            width="100%"
+          />
+          <CardBody>
+            <CardTitle tag="h5">Card title</CardTitle>
+            <CardSubtitle className="mb-2 text-muted" tag="h6">
+              NFT Title
+            </CardSubtitle>
+            <CardText  className="mb-2 text-muted">
+            Art Blocks
+            </CardText>
+            <Button>View details</Button>
+          </CardBody>
+        </Card>
+        <Card>
+          <CardImg
+            alt="Card image cap"
+            src="https://picsum.photos/318/180"
+            top
+            width="100%"
+          />
+          <CardBody>
+            <CardTitle tag="h5">Card title</CardTitle>
+            <CardSubtitle className="mb-2 text-muted" tag="h6">
+              NFT Title
+            </CardSubtitle>
+            <CardText  className="mb-2 text-muted">
+            Art Blocks
+            </CardText>
+            <Button>View details</Button>
+          </CardBody>
+        </Card>
+      </CardGroup>
+    </div>
+  );
+}
+
+{
+  /* <div className="interest_submit-proposal_xyz">
         <span
           className="interest_submit-proposal__image__1JcQq"
           aria-label="Unicorn emoji"
@@ -15,7 +153,5 @@ export default function Collection() {
         <div className="interest_submit-proposal-slogan">
         Back soon for a new collection of trendy NFTS.
         </div>
-      </div>
-    </div>
-  );
+      </div> */
 }
