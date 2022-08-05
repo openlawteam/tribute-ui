9b339dd90039e98f0dfcb8281e8c1ec7560da295
Margotte83 <marjorie.ngoupende@gmail.com>
2022-08-05 12:35:03 +0200

    fix : connect to web3 modal

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -21,7 +21,7 @@ import Transfers from './pages/transfers/Transfers';
 import TributeDetails from './pages/tributes/TributeDetails';
 import Tributes from './pages/tributes/Tributes';
 import Privacy from './pages/privacy/Privacy';
-import Collection from './pages/collection/Collection';
+// import Collection from './pages/collection/Collection';
 
 const proposalIdParameter: string = ':proposalId';
 
@@ -125,12 +125,12 @@ export default function Routes() {
           path="/members/:ethereumAddress"
           render={() => <MemberProfile />}
         />,
-        <Route
-          key="collection"
-          exact
-          path="/collection"
-          render={() => <Collection />}
-        />,
+        // <Route
+        //   key="collection"
+        //   exact
+        //   path="/collection"
+        //   render={() => <Collection />}
+        // />,
         <Route
           key="privacy"
           exact
